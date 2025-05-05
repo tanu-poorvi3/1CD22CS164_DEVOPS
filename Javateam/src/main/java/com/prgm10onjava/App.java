2db3d134b1dbff0a415b3b4325fb75e4b209815a
vijayalaxmi44 <viju808857@gmail.com>
2025-05-05 11:39:44 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/prgm10onjava/App.java
@@ -0,0 +1,13 @@
+package com.prgm10onjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
